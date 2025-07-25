6f842b5e7b78cde4c7162962f02b2ba79666c9d9
Anandhu S <anandhushibu64@gmail.com>
2025-07-25 20:53:36 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = "https://yisdbhbcnmtfrulxinwi.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlpc2RiaGJjbm10ZnJ1bHhpbndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDU2MjIsImV4cCI6MjA2ODUyMTYyMn0.aluz9APowBaUOHOsT-ifVnP6r8p5K7ib7QzHCvS_oNw";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_6HEgYPyIyHADidm753vGDw_GTiseB8H";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
