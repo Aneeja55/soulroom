08c63641524ba152b31ac71d57dfa9e1b714e52f
Anandhu S <anandhushibu64@gmail.com>
2025-07-25 21:38:19 +0530

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   "https://yisdbhbcnmtfrulxinwi.supabase.co",
-  "sb_publishable_6HEgYPyIyHADidm753vGDw_GTiseB8H"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlpc2RiaGJjbm10ZnJ1bHhpbndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDU2MjIsImV4cCI6MjA2ODUyMTYyMn0.aluz9APowBaUOHOsT-ifVnP6r8p5K7ib7QzHCvS_oNw"
 );
